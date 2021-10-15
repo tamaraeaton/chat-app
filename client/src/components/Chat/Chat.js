b6a46819fa1fb9925a9bb767935baf1944373aa3
tamaraeaton <teaton@banyanlabs.io>
2021-10-15 12:59:36 -0700

    added Chat

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -7,6 +7,8 @@ let socket;
 const Chat = ({ location }) => {
   const [name, setName] = useState('');
   const [room, setRoom] = useState('');
+  const [message, setMessage] = useState('');
+  const [messages, setMessages] = useState([]);
   const ENDPOINT = 'localhost:5000'
   
   useEffect(() => {
@@ -23,12 +25,33 @@ const Chat = ({ location }) => {
 
     return () => {
       socket.emit('disconnect');
-
       socket.off();
     }
-  }, [ENDPOINT, location.search])
+  }, [ENDPOINT, location.search]);
+
+  useEffect(() => {
+    socket.on('message', (message) => {
+      setMessages([...messages, message]);
+    })
+  }, [messages]);
+
+  const sendMessage = (event) => {
+    event.preventDefault();
+
+    if(message) {
+      socket.emit('sendMessage', message, () => setMessage(''));
+    }
+  }
+
+  console.log(message, messages);
+
   return (
-    <h1>Chat</h1>
+    <div className = 'outerContainer'>
+      <div className="container">
+        <input value={message} onChange={(event) => setMessage(event.target.value)}
+        onKeyPress={event => event.key === 'Enter' ? sendMessage(event) : null}/>
+      </div>
+    </div>
   )
 }
 
